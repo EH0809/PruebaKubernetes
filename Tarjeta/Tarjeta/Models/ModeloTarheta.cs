2e07db48aa878ed05021c7e0ec5977c7f5fcaab7
Herrera <corpeherrera@consultoria.local>
2024-03-11 13:10:11 -0600

    prueba VS

---
+++ Tarjeta/Tarjeta/Models/ModeloTarheta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tarjeta.Models
+{
+    public class ModeloTarheta
+    {
+
+            public string De { get; set; }
+            public string Para { get; set; }
+            public int Monto { get; set; }
+
+            public string Mensaje { get; set; }
+        public string LinkLargo { get; set; }
+        public string LinkCorto { get; set; }
+
+    }
+}
